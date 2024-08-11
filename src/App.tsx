62a19726268017263eb84b0589dc750cfc952ea1
ofk <ofkjpn+github@gmail.com>
2024-08-11 11:51:48 +0900

    Setup a starter editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { BubbleMenu, EditorProvider, FloatingMenu } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+
+const content = '<p>Hello World!</p>';
+
+const extensions = [StarterKit];
+
 export function App(): React.ReactElement {
-  return <h1>vite-react-ts-boilerplate</h1>;
+  return (
+    <EditorProvider content={content} extensions={extensions}>
+      <FloatingMenu editor={null}>This is the floating menu</FloatingMenu>
+      <BubbleMenu editor={null}>This is the bubble menu</BubbleMenu>
+    </EditorProvider>
+  );
 }
